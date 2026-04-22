67a2fc7afb30fe7f74a08cf8721d12475f761408
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-04-22 09:38:58 -0400

    I created the methods that populate and display the employee objects

--- src/main/java/com/plurasight/Main.java
+++ src/main/java/com/plurasight/Main.java
@@ -1,25 +1,58 @@
 package com.plurasight;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
+    private static Employee[] employees;
     public static void main(String[] args) {
-        String filePath = "src/main/java/com/plurasight/employees.txt";
+        employees = new Employee[8];
+        promptUser();
+    }
+
+    public static void promptUser(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the file employee file to process: ");
+        String employeeFileName = scanner.nextLine();
+        String filePath = employeeFileName;
+        readFile(filePath);
+        System.out.print("Enter the name of the payroll file to create: ");
+        String payrollFileName = scanner.nextLine();
+        writeFile(payrollFileName);
+    }
+    public static void writeFile(String payrollFileName){
+        try {
+            FileWriter fileWriter = new FileWriter(payrollFileName);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("id|" + "name|" + "gross pay \n");
+            String fileContent = "";
+            for(Employee employee: employees){
+                fileContent = String.format("%d| %s| $%.2f \n",employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+                bufferedWriter.write(fileContent);
+            }
+            System.out.println("File created successfully");
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void readFile(String filePath) {
         System.out.println("================================\n"
                 + "       Employee Dataset\n" +
                 "================================");
-        loadFile(filePath);
-    }
-
-    public static void loadFile(String filePath) {
+        System.out.println("Employee ID| Name| Gross Pay");
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
             String inputFile;
+            int index = -1;
             while ((inputFile = bufferedReader.readLine()) != null) {
-                populateEmployee(inputFile);
+                if (index >= 0) {
+                    Employee employee = populateEmployee(inputFile);
+                    employees[index] = employee;
+                }
+                index++;
             }
             bufferedReader.close();
         } catch (FileNotFoundException e) {
@@ -40,7 +73,7 @@ public class Main {
         employee.setHoursWorked(hoursWorked);
         double payRate = Double.parseDouble(fileParts[3]);
         employee.setPayRate(payRate);
-        System.out.printf("employee ID: %d, name: %s, gross pay: $%.2f \n",employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+        System.out.printf("%d| %s| $%.2f \n",employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
         return employee;
     }
 }
